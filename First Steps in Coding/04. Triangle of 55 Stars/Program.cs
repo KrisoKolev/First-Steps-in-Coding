3500cf3148da26507da1eae17564d6f5c8988038
KrisoKolev <kristian.kolevv@gmail.com>
2017-01-18 13:05:52 +0200

    The first homework from "Programming Basic".

---
+++ First Steps in Coding/04. Triangle of 55 Stars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int k = 0; k <= i; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ First Steps in Coding/06. Square of Stars/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string top = new string('*', n);
+        string whiteSpaces = new string(' ', n - 2);
+
+        Console.WriteLine(top);
+        for (int i = 0; i < n - 2; i++)
+        {
+            Console.WriteLine("*" + whiteSpaces + "*");
+        }
+        Console.WriteLine(top);
+    }
+}
+

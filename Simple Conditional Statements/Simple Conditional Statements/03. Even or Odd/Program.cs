506994a0b096f9b5cf3e243b4afc683f436658e5
KrisoKolev <kristian.kolevv@gmail.com>
2017-01-19 20:22:35 +0200

    Update with Simple Conditional Statements

---
+++ Simple Conditional Statements/Simple Conditional Statements/03. Even or Odd/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int digit = int.Parse(Console.ReadLine());
+        if (digit % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        else
+        {
+            Console.WriteLine("Odd");
+        }
+    }
+}
---
+++ Simple Conditional Statements/Simple Conditional Statements/11. Equal Words/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string wordOne = Console.ReadLine().ToLower();
+        string wordTwo = Console.ReadLine().ToLower();
+
+        if (wordOne == wordTwo)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
+
